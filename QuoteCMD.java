277278add342b992cc37b7cb669953111a196384
EthanTPainter <ethantpainter@gmail.com>
2018-02-03 18:26:18 -0500

    Added functionality for Searching quot list

--- QuoteCMD.java
+++ QuoteCMD.java
@@ -5,90 +5,29 @@
  * Does not interact with remote server.
  */
 
-import org.xml.sax.Attributes;
-import org.xml.sax.SAXException;
-import org.xml.sax.helpers.DefaultHandler;
-
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
-import java.util.ArrayList;
-import java.util.Random;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 //Any imports
 public class QuoteCMD{
 
-    public static void main(String[] args)
+    //Added "throws IOException" because of Buffered Reader
+    public static void main(String[] args) throws IOException
     {
         //Variables
         int loopValue = -1;
-        String searchTerm = new String();
+        String searchTerm;
+        QuoteList searchList;
+        Quote searchQuote;
         String[] myLastFiveSearches = {"","","","",""};
-        ArrayList<String> quotes = new ArrayList<String>();
-        ArrayList<String> authors = new ArrayList<String>();
-        Random randomGen = new Random(915079);
+        int myLastFiveSearchesCounter = 0;
         QuoteList quoteList;
-        //Scanner for CMD line input
-        Scanner sc = new Scanner(System.in);
+        //BufferedReader for CMD line input
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         //Read XML Files
-        try {
-            SAXParserFactory factory = SAXParserFactory.newInstance();
-            SAXParser saxParser = factory.newSAXParser();
-
-            DefaultHandler handler = new DefaultHandler(){
-                boolean quote = false;
-                boolean author = false;
-
-                public void startElement(String uri, String localName, String qName, Attributes atts)
-                        throws SAXException
-                {
-                    //Debugging for start element ("<" + term + ">)
-                    //System.out.println("Start Element: "+  qName);
-
-                    if(qName.equals("quote-text")) {
-                        quote = true;
-                    }
-                    else if(qName.equals("author")) {
-                        author = true;
-                    }
-                }
-
-                public void endElement(String uri, String localName, String qName)
-                        throws SAXException
-                {
-                    //Debugging for end element ("</" + term + ">")
-                    //System.out.println("End Element: " + qName);
-                }
-
-                public void characters(char chars[], int start, int length)
-                    throws SAXException
-                {
-                    String result = new String(chars, start, length);
-                    if(quote){
-                        System.out.println("Quote: " + result);
-                        quotes.add(result);
-                        quote = false;
-                    }
-                    else if(author){
-                        System.out.println("Author: " + new String(chars, start, length));
-                        authors.add(result);
-                        author = false;
-                    }
-                }
-            };
-            // Location of Quotes.xml file locally
-            //saxParser.parse("C:/Users/Ethan/Desktop/GMU/2018 Spring Classes/Quotes-Testing-437/quotes.xml", handler);
-        }
-        catch(Exception e){
-            e.printStackTrace();
-        }
-
         QuoteSaxParser quoteSaxParser = new QuoteSaxParser("C:/Users/Ethan/Desktop/GMU/2018 Spring Classes/Quotes-Testing-437/quotes.xml");
         quoteList = quoteSaxParser.getQuoteList();
-
-        System.out.println("\nQuotes.size: " + quoteList.getSize());
-        System.out.println("Authors.size: " + quoteList.getSize());
-
         while(loopValue != 0)
         {
             //Search Functionality (Current existing quotes)
@@ -101,7 +40,7 @@ public class QuoteCMD{
             System.out.println("3) Search For Either Existing Quote or Author");
             System.out.println("4) Generate a Random Quote");
             System.out.println("5) Print last 5 Search Terms");
-            loopValue = sc.nextInt();
+            loopValue = Integer.parseInt(br.readLine());
             //Loop through loop with input
             switch(loopValue){
                 case 0:
@@ -109,25 +48,73 @@ public class QuoteCMD{
                     System.out.println("\nQuitting...");
                     break;
                 case 1:
-                    //Term(s) in quote
+                    //Search term(s) in quotes
                     System.out.println("\nEnter term(s) to search for in a quote: ");
-                    QuoteList newList = new QuoteList();
+                    searchTerm = br.readLine();
+                    //Send input to search method
+                    searchList = quoteList.search(searchTerm,1);
+                    //Nothing found in search list (quotes)
+                    if(searchList.getSize() == 0)
+                    {
+                        System.out.println("Term(s): \"" + searchTerm + "\" not found in current quote list");
+                    }
+                    //At least one search term found (quotes)
+                    else{
+                        for(int i = 0; i < searchList.getSize(); i++)
+                        {
+                            searchQuote = searchList.getQuote(i);
+                            System.out.println("Quote:  " + searchQuote.getQuoteText());
+                            System.out.println("Author: " + searchQuote.getAuthor() + "\n");
+                        }
+                    }
                     break;
                 case 2:
-                    //Term(s) in author
+                    //Search term(s) in authors
                     System.out.println("\nEnter term(s) to search for in an author's name: ");
-
+                    searchTerm = br.readLine();
+                    //Send input to search method
+                    searchList = quoteList.search(searchTerm, 0);
+                    //Nothing found in search list (authors)
+                    if(searchList.getSize() == 0)
+                    {
+                        System.out.println("Terms: \"" + searchTerm + "\" not found in current author list");
+                    }
+                    //At least one search term found (authors)
+                    else{
+                        for(int i = 0; i < searchList.getSize(); i++)
+                        {
+                            searchQuote = searchList.getQuote(i);
+                            System.out.println("Quote:  " + searchQuote.getQuoteText());
+                            System.out.println("Author: " + searchQuote.getAuthor() + "\n");
+                        }
+                    }
                     break;
                 case 3:
-                    //Term(s) in quote/author
+                    //Search term(s) in quotes or authors
                     System.out.println("\nEnter term(s) to search for in an author or quote: ");
-
+                    searchTerm = br.readLine();
+                    //Send input to search method
+                    searchList = quoteList.search(searchTerm, 2);
+                    //Nothing found in search list (quotes or authors)
+                    if(searchList.getSize() == 0)
+                    {
+                        System.out.println("Terms: \"" + searchTerm + "\" not found in current quote or author lists");
+                    }
+                    //At least one search term found (quotes or authors)
+                    else{
+                        for(int i = 0; i < searchList.getSize(); i++)
+                        {
+                            searchQuote = searchList.getQuote(i);
+                            System.out.println("Quote:  " + searchQuote.getQuoteText());
+                            System.out.println("Author: " + searchQuote.getAuthor() + "\n");
+                        }
+                    }
                     break;
                 case 4:
                     //Generate a random quote
                     System.out.println("\nGenerating a random Quote...");
                     Quote quoteTmp = quoteList.getRandomQuote();
-                    System.out.println(quoteTmp.getQuoteText() + "\n\t-" + quoteTmp.getAuthor() + "\n");
+                    System.out.println("Quote:  " + quoteTmp.getQuoteText() + "\nAuthor: " + quoteTmp.getAuthor() + "\n");
                     break;
                 case 5:
                     //Print last 5 Search Terms
