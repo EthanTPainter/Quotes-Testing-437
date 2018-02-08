2a2c02f353a809ce2be0ce4bccad355d67d08780
Ethan <ethantpainter@gmail.com>
2018-02-07 20:01:14 -0500

    Refactor cases (#1)
    * Refactored add quote method outside of main
    
    * Added more comments to specify default xml quotes
    
    * Adding .close for bufferedReader
    
    * Refactored Switch statements to increase readability
    
    * Finished refactoring switch statements

--- QuoteCMD.java
+++ QuoteCMD.java
@@ -1,36 +1,51 @@
 /* @author: Ethan Painter
  * Created Date: 2/1/17
+ * Config: Java 1.8
+ *
  * quoteCMD created as a command line interface for quotesServe
  * Does not communicate with remote server (stand alone interface)
  * All interactions are recorded and displayed using only client side
- * Does not interact with remote server.
  */
 
+//Any imports
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-//Any imports
 public class QuoteCMD{
+
+    //Variables
+    private String searchTerm;
+    private String[] myLastFiveSearches = {"","","","",""};
+    private int myLastFiveSearchesCounter = 0;
+    private QuoteList searchList;
+    private Quote searchQuote;
+    //BufferedReader for CMD line input
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    //Read XML Files
+    QuoteSaxParser quoteSaxParser = new QuoteSaxParser("C:\\Users\\EthanPC\\Desktop\\GMU\\Quotes-Testing-437\\quotes.xml");
+    QuoteList quoteList = quoteSaxParser.getQuoteList();
+
+    //Getter for quoteList
+    public QuoteList getQuoteList() {
+        return quoteList;
+    }
+
+    //Closes buffered reader br when quitting
+    public void closeBufferedReader() throws IOException {
+        br.close();
+    }
 
     //Added "throws IOException" because of Buffered Reader
     public static void main(String[] args) throws IOException
     {
-        //Variables
+        //Make QuoteCMD runner to use methods included
+        QuoteCMD runner = new QuoteCMD();
         int loopValue = -1;
-        String searchTerm;
-        QuoteList searchList;
-        Quote searchQuote;
-        String[] myLastFiveSearches = {"","","","",""};
-        int myLastFiveSearchesCounter = 0;
-        QuoteList quoteList;
-        //BufferedReader for CMD line input
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        //Read XML Files
-        QuoteSaxParser quoteSaxParser = new QuoteSaxParser("C:/Users/Ethan/Desktop/GMU/2018 Spring Classes/Quotes-Testing-437/quotes.xml");
-        quoteList = quoteSaxParser.getQuoteList();
         while(loopValue != 0)
         {
+            //BufferedReader for CMD line input
+            BufferedReader sample = new BufferedReader(new InputStreamReader(System.in));
             //Search Functionality (Current existing quotes)
             //Search by Quote, Author, or both
             //Show Recent Searches (Just User Searches)
@@ -42,163 +57,42 @@ public class QuoteCMD{
             System.out.println("4) Add quote to current quote list");
             System.out.println("5) Generate a Random Quote");
             System.out.println("6) Print last 5 Search Terms");
-            loopValue = Integer.parseInt(br.readLine());
+            loopValue = Integer.parseInt(sample.readLine());
             //Loop through loop with input
             switch(loopValue){
                 case 0:
-                    //Quit
+                    //Quit & Close Buffered Readers
+                    sample.close();
+                    runner.closeBufferedReader();
                     System.out.println("\nQuitting...");
                     break;
                 case 1:
-                    //Search term(s) in quotes
-                    System.out.println("\nEnter term(s) to search for in a quote: ");
-                    searchTerm = br.readLine();
-                    //If room in list, update it
-                    if(myLastFiveSearchesCounter < 5)
-                    {
-                        myLastFiveSearches[myLastFiveSearchesCounter] = searchTerm;
-                        myLastFiveSearchesCounter++;
-                    }
-                    //Max Counter Reached
-                    else{
-                        //Remove Earliest indexed term to make room for the new term
-                        for(int i = 0; i < 4; i++)
-                        {
-                            myLastFiveSearches[i] = myLastFiveSearches[i+1];
-                        }
-                        myLastFiveSearches[myLastFiveSearchesCounter-1] = searchTerm;
-                    }
-                    //Send input to search method
-                    searchList = quoteList.search(searchTerm,1);
-                    //Nothing found in search list (quotes)
-                    if(searchList.getSize() == 0)
-                    {
-                        System.out.println("\nTerm(s): \"" + searchTerm + "\" not found in current quote list\n");
-                    }
-                    //At least one search term found (quotes)
-                    else{
-                        for(int i = 0; i < searchList.getSize(); i++)
-                        {
-                            searchQuote = searchList.getQuote(i);
-                            System.out.println("Quote:  " + searchQuote.getQuoteText());
-                            System.out.println("Author: " + searchQuote.getAuthor() + "\n");
-                        }
-                    }
+                    //Mode = 1
+                    runner.searchQuotes(1);
                     break;
                 case 2:
-                    //Search term(s) in authors
-                    System.out.println("\nEnter term(s) to search for in an author's name: ");
-                    searchTerm = br.readLine();
-                    //If room in list, update it
-                    if(myLastFiveSearchesCounter < 5)
-                    {
-                        myLastFiveSearches[myLastFiveSearchesCounter] = searchTerm;
-                        myLastFiveSearchesCounter++;
-                    }
-                    //Max Counter Reached
-                    else{
-                        //Remove Earliest indexed term to make room for the new term
-                        for(int i = 0; i < 4; i++)
-                        {
-                            myLastFiveSearches[i] = myLastFiveSearches[i+1];
-                        }
-                        myLastFiveSearches[myLastFiveSearchesCounter-1] = searchTerm;
-                    }
-                    //Send input to search method
-                    searchList = quoteList.search(searchTerm, 0);
-                    //Nothing found in search list (authors)
-                    if(searchList.getSize() == 0)
-                    {
-                        System.out.println("\nTerm(s): \"" + searchTerm + "\" not found in current author list\n");
-                    }
-                    //At least one search term found (authors)
-                    else{
-                        for(int i = 0; i < searchList.getSize(); i++)
-                        {
-                            searchQuote = searchList.getQuote(i);
-                            System.out.println("Quote:  " + searchQuote.getQuoteText());
-                            System.out.println("Author: " + searchQuote.getAuthor() + "\n");
-                        }
-                    }
+                    //Mode = 0
+                    runner.searchQuotes(0);
                     break;
                 case 3:
-                    //Search term(s) in quotes or authors
-                    System.out.println("\nEnter term(s) to search for in an author or quote: ");
-                    searchTerm = br.readLine();
-                    //If room in list, update it
-                    if(myLastFiveSearchesCounter < 5)
-                    {
-                        myLastFiveSearches[myLastFiveSearchesCounter] = searchTerm;
-                        myLastFiveSearchesCounter++;
-                    }
-                    //Max Counter Reached
-                    else{
-                        //Remove Earliest indexed term to make room for the new term
-                        for(int i = 0; i < 4; i++)
-                        {
-                            myLastFiveSearches[i] = myLastFiveSearches[i+1];
-                        }
-                        myLastFiveSearches[myLastFiveSearchesCounter-1] = searchTerm;
-                    }
-                    //Send input to search method
-                    searchList = quoteList.search(searchTerm, 2);
-                    //Nothing found in search list (quotes or authors)
-                    if(searchList.getSize() == 0)
-                    {
-                        System.out.println("\nTerm(s): \"" + searchTerm + "\" not found in current quote or author lists\n");
-                    }
-                    //At least one search term found (quotes or authors)
-                    else{
-                        for(int i = 0; i < searchList.getSize(); i++)
-                        {
-                            searchQuote = searchList.getQuote(i);
-                            System.out.println("Quote:  " + searchQuote.getQuoteText());
-                            System.out.println("Author: " + searchQuote.getAuthor() + "\n");
-                        }
-                    }
+                    //Mode = 2
+                    runner.searchQuotes(2);
                     break;
                 case 4:
-                    String newQuoteText;
-                    String newAuthor;
-                    Quote newQuote;
-                    System.out.print("\nPrepare to add Quote");
-                    //Get input for quoteText and author from user
-                    System.out.print("\nQuote: ");
-                    newQuoteText = br.readLine();
-                    System.out.print("Author: ");
-                    newAuthor = br.readLine();
-                    //If quote is empty, return an error message and return to menu
-                    if(newQuoteText.equals("")){
-                        System.out.println("\nError: Quote text provided is empty (No quote provided)\n");
-                        break;
-                    }
-                    //If author is empty, make a author anonymous (refers to Constructor I designed in assignment #2)
-                    if(newAuthor.equals("")){
-                        newQuote = new Quote(newQuoteText);
+                    if (addQuote(runner.getQuoteList())){
+                        System.out.println("\nSuccessfully Added new Quote!\n");
                     }
                     else{
-                        newQuote = new Quote(newAuthor, newQuoteText);
+                        System.out.println("\nError: Quote text provided is empty (No quote provided)\n");
                     }
-                    //Add quote to the current quote list (temporarily)
-                    //After session closes, quote is lost/removed. Only xml data is saved
-                    //Would have to add to xml data file to make permanent
-                    quoteList.setQuote(newQuote);
-                    System.out.println("Successfully Added new Quote!\n");
                     break;
                 case 5:
                     //Generate a random quote
-                    System.out.println("\nGenerating a random Quote...");
-                    Quote quoteTmp = quoteList.getRandomQuote();
-                    System.out.println("Quote:  " + quoteTmp.getQuoteText() + "\nAuthor: " + quoteTmp.getAuthor() + "\n");
+                    runner.generateRandomQuote();
                     break;
                 case 6:
                     //Print last 5 Search Terms
-                    System.out.println("\nPrinting last 5 Search Terms: ");
-                    for(int i = 0; i < myLastFiveSearchesCounter; i++)
-                    {
-                        System.out.println(i+1 +") " + myLastFiveSearches[i]);
-                    }
-                    System.out.println();
+                    runner.printLastFiveSearchTerms();
                     break;
                 default:
                     System.out.println("\nError: menu number not detected");
@@ -206,5 +100,89 @@ public class QuoteCMD{
                     break;
             }
         }
+    }
+
+    public void searchQuotes(int mode) throws IOException
+    {
+        //Search term(s) in quotes
+        System.out.println("\nEnter term(s) to search for in a quote: ");
+        searchTerm = br.readLine();
+        //If room in list, update it
+        if(myLastFiveSearchesCounter < 5)
+        {
+            myLastFiveSearches[myLastFiveSearchesCounter] = searchTerm;
+            myLastFiveSearchesCounter++;
+        }
+        //Max Counter Reached
+        else{
+            //Remove Earliest indexed term to make room for the new term
+            for(int i = 0; i < 4; i++)
+            {
+                myLastFiveSearches[i] = myLastFiveSearches[i+1];
+            }
+            myLastFiveSearches[myLastFiveSearchesCounter-1] = searchTerm;
+        }
+        //Send input to search method
+        searchList = quoteList.search(searchTerm, mode);
+        //Nothing found in search list (quotes)
+        if(searchList.getSize() == 0)
+        {
+            System.out.println("\nTerm(s): \"" + searchTerm + "\" not found in current quote list\n");
+        }
+        //At least one search term found (quotes)
+        else{
+            for(int i = 0; i < searchList.getSize(); i++)
+            {
+                searchQuote = searchList.getQuote(i);
+                System.out.println("Quote:  " + searchQuote.getQuoteText());
+                System.out.println("Author: " + searchQuote.getAuthor() + "\n");
+            }
+        }
+    }
+
+    public static boolean addQuote(QuoteList list) throws IOException {
+        String newQuoteText;
+        String newAuthor;
+        Quote newQuote;
+        BufferedReader newBR = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("\nPrepare to add Quote");
+        //Get input for quoteText and author from user
+        System.out.print("\nQuote: ");
+        newQuoteText = newBR.readLine();
+        System.out.print("Author: ");
+        newAuthor = newBR.readLine();
+        //If quote is empty, return an error message and return to menu
+        if(newQuoteText.equals("")){
+            return false;
+        }
+        //If author is empty, make a author anonymous (refers to Constructor I designed in assignment #2)
+        if(newAuthor.equals("")){
+            newQuote = new Quote(newQuoteText);
+        }
+        else{
+            newQuote = new Quote(newAuthor, newQuoteText);
+        }
+        //Add quote to the current quote list (permanent)
+        list.setQuote(newQuote);
+        //After session closes, quote is lost/removed. Only xml data is saved
+        //Would have to add to xml data file to make permanent
+        
+        return true;
+    }
+
+    public void generateRandomQuote()
+    {
+        System.out.println("\nGenerating a random Quote...");
+        Quote quoteTmp = quoteList.getRandomQuote();
+        System.out.println("Quote:  " + quoteTmp.getQuoteText() + "\nAuthor: " + quoteTmp.getAuthor() + "\n");
+    }
+    public void printLastFiveSearchTerms()
+    {
+        System.out.println("\nPrinting last 5 Search Terms: ");
+        for(int i = 0; i < myLastFiveSearchesCounter; i++)
+        {
+            System.out.println(i+1 +") " + myLastFiveSearches[i]);
+        }
+        System.out.println();
     }
 }
